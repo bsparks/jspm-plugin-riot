8285cd8809eb793294411547baf90f2532a8d348
Ben Sparks <bsparks42@gmail.com>
2016-01-13 13:08:08 -0700

    converted to commonjs format as that seems to make the bundle work

--- tag.js
+++ tag.js
@@ -1,5 +1,10 @@
 var compiler = require('riot-compiler');
 
 module.exports.translate = function(load) {
-    return compiler.compile(load.source);
+    load.metadata.format = 'cjs';
+    var compiledtag = compiler.compile(load.source);
+    //console.log(compiledtag);
+    var output = 'var riot = require("riot");\n' + compiledtag;
+
+    return output;
 };
